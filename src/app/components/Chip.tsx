89b9dc707b345dfb2e8e13ccd16fa234ce12dcf3
marialuiza2a <mluizasenai2@gmail.com>
2023-12-29 12:46:57 -0300

    feat: acrepan

--- src/app/components/Chip.tsx
+++ src/app/components/Chip.tsx
@@ -4,18 +4,19 @@ import React from "react";
 
 interface ChipProps {
   label: string;
-  onClick?: () => void;
-  active: boolean;
 }
 
-export function Chip({ label, onClick, active }: ChipProps) {
+export function Chip({ label }: ChipProps) {
+
+  const active = false;
+
   return (
     <div
       className={`cursor-pointer inline-block px-4 py-2 rounded ${
-        active ? "bg-[#EAA85C] text-black font-semibold" : "bg-[#303030] text-white"
+        active
+          ? "bg-[#EAA85C] text-black font-semibold"
+          : "bg-[#303030] text-white"
       }`}
-      onClick={onClick}
-      data-chip-id={label}
     >
       {label}
     </div>
