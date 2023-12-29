92b045351d4894355b866ef2cd3bb3246f3012ee
marialuiza2a <mluizasenai2@gmail.com>
2023-12-29 13:25:40 -0300

    feat: add acrepan image with login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import Link from "next/link";
-
 import { LoginForm } from "./components/loginForm";
 
 export default function authPage() {
   return (
     <section className="w-screen h-screen p-10">
-      <Link
-        href="#"
-        className="text-[#EAA85C] text-md font-bold flex w-full items-center justify-end"
-      >
-        Continuar como visitante
-      </Link>
       <LoginForm />
     </section>
   );
